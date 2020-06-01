da28996ff5cc546959073083bc19223b81b83655
Mesut Öncel <mesutoncel91@gmail.com>
2020-06-01 19:42:44 +0300

    deleted print

--- adapter.py
+++ adapter.py
@@ -2,6 +2,5 @@ from client import Client
 
 class Adapter:
     async def generate_user(self, params, *args, **kwargs):
-        print('adapter')
         response = await Client().generate_user(**params)
         return response
